6694cadeb574f56c60203bb44311cbe80ca6d18a
DNathanael19 <115087435+DNathanael19@users.noreply.github.com>
2023-03-10 20:12:14 -0300

    .

--- Py/QrCodeTest.py
+++ Py/QrCodeTest.py
@@ -9,17 +9,21 @@ from pyzbar.pyzbar import decode
 video = cv2.VideoCapture(0)
 
 #Definindo altura e largura:
-video.set(3, 640)
-video.set(4, 480)
+video.set(3, 700)
+video.set(4, 500)
 
 while True:
-    sucess, qrcode = video.read()
-    for barcode in decode(qrcode):
+    sucess, ler = video.read()
+    for barcode in decode(ler):
         myData = barcode.data.decode("utf-8")
         print(myData)
-        pontos = np.array([barcode.polygon], np.int32)
+        pontos = np.array([barcode.polygon], np.int64)
         pontos = pontos.reshape((-1, 1, 2))
-        cv2.polylines(qrcode, [pontos], True, (255, 0, 0), 5)
+        cv2.polylines(ler, [pontos], True, (255, 0, 0), 2)
+        pontos2 = barcode.rect
+        cv2.putText(ler, myData, (pontos2[0], pontos2[1]), cv2.FONT_HERSHEY_SIMPLEX,
+                                     0.9, (255, 0, 0), 2)
 
-    cv2.imshow("Janela", qrcode)
+    cv2.imshow("Janela", ler)
     cv2.waitKey(1)
+
