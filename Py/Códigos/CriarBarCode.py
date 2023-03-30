77a37d0fd0310451188c5739d23f4c3e49feb313
diego-nathanael <115087435+DNathanael19@users.noreply.github.com>
2023-03-30 08:47:43 -0300

    Delete Py directory

--- Py/Códigos/CriarBarCode.py
+++
@@ -1,9 +0,0 @@
-from barcode import Code128
-from barcode.writer import ImageWriter
-
-lista_alunos = {"Diego Cavalcante": "20211321000", "Rayssa Damacieira":"20211321001"}
-
-for percorre in lista_alunos:
-    codigo = lista_alunos[percorre]
-    cod = Code128(codigo, writer=ImageWriter())
-    cod.save(f"barcod_{percorre}")
--- Py/Códigos/CriarQrCode.py
+++
@@ -1,4 +0,0 @@
-import qrcode
-
-img_qrcode = qrcode.make("https://dnathanael19.github.io/CARTAO_RESPOSTAS/")
-img_qrcode.save("qrcode.png")
